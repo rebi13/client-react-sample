fc53decb0dcaa278ce3ab5aacd747dd8e786d78d
rebi13 <rebi13@naver.com>
2023-11-01 16:23:47 +0900

    feat: 라우터 적용 및 설정

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,23 +1,23 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
 
-import App from "../App";
-import Home from "../pages/Home/Home";
-import List from "../pages/Board/List/List";
-import Detail from "../pages/Board/Detail/Detail";
-import Post from "../pages/Board/Post/Post";
+import App from '../App';
+import Home from '../pages/Home/Home';
+import List from '../pages/Board/List/List';
+import Detail from '../pages/Board/Detail/Detail';
+import Post from '../pages/Board/Post/Post';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
-      { index: true, path: "/", element: <Home /> },
-      { path: "/list", element: <List /> },
-      { path: "/list/:id", element: <Detail /> },
-      { path: "/post", element: <Post /> },
-      { path: "/post/:id", element: <Post /> },
-    ],
-  },
+      { index: true, path: '/', element: <Home /> },
+      { path: '/list', element: <List /> },
+      { path: '/detail/:id', element: <Detail /> },
+      { path: '/post', element: <Post /> },
+      { path: '/post/:id', element: <Post /> }
+    ]
+  }
 ]);
 
 export default router;
