16dba55e338db4e4f748d6d615c25863a826a048
rebi13 <rebi13@naver.com>
2023-11-01 16:26:18 +0900

    fix: list에 react-query 적용

--- src/pages/Board/List/List.tsx
+++ src/pages/Board/List/List.tsx
@@ -1,3 +1,35 @@
+import { useQuery } from '@tanstack/react-query';
+import { Link } from 'react-router-dom';
+import { res, Board } from '../../../@types';
+import axiosRequest from '../../../api/index';
+
 export default function List() {
-  return <>List</>;
+  const {
+    isLoading,
+    error,
+    data: board
+  } = useQuery({
+    queryKey: ['boardList'],
+    queryFn: () => axiosRequest.requestAxios<res<Board[]>>('get', '/boards')
+  });
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) return 'An error has occurred: ' + error;
+
+  if (board) {
+    return (
+      <h2>
+        {board.data.map((el) => (
+          <div>
+            title: <Link to={`/Detail/${el._id}`}>{el.title}</Link> | content:{' '}
+            {el.content}
+          </div>
+        ))}
+      </h2>
+    );
+  }
+  return <div>No data available</div>; // 데이터가 없는 경우 예외 처리
 }
