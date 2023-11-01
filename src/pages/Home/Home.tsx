a7ec9e4e83633ae9c9483c7622e56a19cb1369dc
rebi13 <rebi13@naver.com>
2023-11-01 16:25:56 +0900

    fix: home에 list 접근 링크 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-  return <>Home</>;
+  return (
+    <>
+      <div>Home</div>
+      <div>
+        <Link to={"/list"}>List</Link>
+      </div>
+    </>
+  );
 }
