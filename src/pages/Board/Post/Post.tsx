17bb79cb15e9dfe1099112460e986124b270df47
rebi13 <rebi13@naver.com>
2023-11-07 21:12:21 +0900

    fix: useQuery, useMutation 커스텀 훅 적용

--- src/pages/Board/Post/Post.tsx
+++ src/pages/Board/Post/Post.tsx
@@ -1,79 +1,80 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import useCustomMutation from '../../../hooks/useCustomMutation';
-import axiosRequest from '../../../api';
 import { Board, res } from '../../../@types';
+import useCustomMutation from '../../../hooks/useCustomMutation';
+import useCustomQuery from '../../../hooks/useCustomQuery';
 
 export default function Post() {
   const { id } = useParams();
-  //input form 참조
+
+  // input form 참조
   const titleRef = useRef<HTMLInputElement>(null);
   const contentRef = useRef<HTMLTextAreaElement>(null);
 
-  const {
-    isLoading,
-    isError,
-    error,
-    data: board
-  } = useQuery({
-    queryKey: ['getBoardForUpdate', id],
-    queryFn: () =>
-      id
-        ? axiosRequest.requestAxios<res<Board>>('get', `/boards/${id}`)
-        : undefined,
-    enabled: !!id // id 존재할 경우에만 수행
-  });
-
   const key = ['posts']; // Key 값 설정
-  const info = {
+  const [info, setInfo] = useState({
     method: id ? 'patch' : 'post',
     url: id ? `/boards/${id}` : '/boards',
-    data: { title: titleRef, content: contentRef }
-  };
+    data: { title: '', content: '' }
+  });
 
-  const CustomMutation = useCustomMutation<res<Board>>(key, info);
+  // 기존 board 데이터가 남아있어서 등록 들어갔을 때 데이터가 그대로 보임
+  const { data: board } = useCustomQuery<res<Board>>(['getBoardForUpdate'], {
+    method: 'get',
+    url: `/boards/${id}`,
+    enabled: !!id
+  });
 
-  const mutation = CustomMutation();
-
-  // const mutation = useCustomMutation<res<Board>>(key, info);
+  const mutation = useCustomMutation<res<Board>>(key, info);
 
   // id가 변경될 때마다 데이터를 가져와서 titleRef와 contentRef에 할당합니다.
   useEffect(() => {
-    if (id && titleRef.current && contentRef.current && board) {
-      titleRef.current.value = board.data.title;
-      contentRef.current.value = board.data.content;
+    if (id && board) {
+      setInfo((prevInfo) => ({
+        ...prevInfo,
+        data: { title: board.data.title, content: board.data.content }
+      }));
+    } else {
+      setInfo({
+        method: 'post',
+        url: '/boards',
+        data: { title: '', content: '' }
+      });
     }
-  }, [id, board]);
-
-  // 수정 api 호출하는거 처리하고 그담에 react query custom hook으로 만들기 공통으로
-  // 조건부 렌더링을 수행합니다.
-  if (id) {
-    if (isLoading) {
-      return <div>Loading...</div>;
-    }
-
-    if (isError) {
-      return <h2>{'An error has occurred: ' + error}</h2>;
-    }
-  }
+  }, [id, board, setInfo]);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // TODO: 제목, 내용 유효성 검사
     const title = titleRef.current?.value || '';
     const content = contentRef.current?.value || '';
-    mutation.mutate();
+    if (title.length && content.length) {
+      setInfo({
+        ...info,
+        data: { title, content }
+      });
+      mutation.mutate();
+    } else {
+      alert('제목과 내용을 입력하세요.');
+    }
   };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
         <div>
-          <input type="text" placeholder="제목" ref={titleRef} />
+          <input
+            type="text"
+            placeholder="제목"
+            ref={titleRef}
+            defaultValue={info.data.title}
+          />
         </div>
         <div>
-          <textarea placeholder="내용" ref={contentRef} />
+          <textarea
+            placeholder="내용"
+            ref={contentRef}
+            defaultValue={info.data.content}
+          />
         </div>
         <button type="submit" disabled={mutation.isLoading}>
           {id ? '수정' : '등록'}
